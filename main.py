988847f56d652cb33da5cc0ea5b7b865d9810414
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-04 20:19:10 +0530

    added my_work section(title, content, images and source code)

--- main.py
+++ main.py
@@ -20,12 +20,18 @@ st.title("My Work:", )
 
 df = pandas.read_csv("data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.subheader(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.subheader(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
