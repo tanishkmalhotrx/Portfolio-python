1ba57dc4232809275c7d9e13ba841b3167465f69
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-04 21:27:55 +0530

    send email func

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Email address")
+    user_message = st.text_area("Message")
+    message = f"""\
+    Subject: My_website Mail from {user_email}
+    From: {user_email}
+    {user_message}
+    """
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(user_message)
+        st.info("Your Email was sent successfully!")
---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib, ssl
+
+def send_email(user_message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "tanishkmalhotra190804@gmail.com"
+    password = "oowhexnrclaawuhx"
+
+    receiver = "tanishkmalhotra190804@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, user_message)
