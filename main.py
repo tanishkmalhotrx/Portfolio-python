eb655b0fa4f5685ec0ccc79362ba82263baf545a
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-04 19:56:36 +0530

    added project titles

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=450)
 
 with col2:
     st.title("Tanishk Malhotra")
@@ -14,3 +15,17 @@ with col2:
     working with python, HTML, CSS, JS.
     """
     st.info(content)
+
+st.title("My Work:", )
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.subheader(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.subheader(row["title"])
